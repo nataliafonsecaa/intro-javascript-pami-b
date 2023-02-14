8ba6c55b49a01df801d173092454d2cf49a28893
nataliafonsecaa <natalia.fonseca8@etec.sp.gov.br>
2023-02-13 22:49:41 -0300

    Aula Intro
    Aula 1 - Introdução de variáveis, concatenação e  tipagem.

---
+++ exemplosjs/001 - helloWorld.js
@@ -0,0 +1,27 @@
+// comentário de uma linha
+console.log('Estou desenvolvendo em java-script');
+ 
+/*
+comentário de mais de 
+uma linha
+*/
+let nome = "Natalia";
+let idade = 28;
+let trabalha = true;
+let altura_pessoa = 1.68;
+let cores_fav = ["preto" , "azul" , "roxo"]
+let endereco = {
+    logadouro: "Rua", 
+    numlog: "Humaitá",
+    num: "666", 
+    bairro: "Vila Independencia"
+};
+let covid = null;
+let dengue;
+
+console.log(altura_pessoa);
+console.log("Nome: " + nome);
+console.log("A " + nome + " tem " +  idade  + " anos. ");
+console.log('');;
+console.log("------------------------------------");
+console.log(typeof(dengue));
